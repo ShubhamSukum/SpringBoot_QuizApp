ade4c1c67f7dc18617e5de4a8643d97741e87876
ShubhamSukum <shubhamsukum1722002@gmail.com>
2024-01-20 22:10:00 +0530

    Monolithic Application✔

--- src/main/java/com/shubham/quizapp/controller/QuizController.java
+++ src/main/java/com/shubham/quizapp/controller/QuizController.java
@@ -1,14 +1,14 @@
 package com.shubham.quizapp.controller;
 
+import com.shubham.quizapp.model.QuestionWrapper;
 import com.shubham.quizapp.model.Quiz;
+import com.shubham.quizapp.model.Response;
 import com.shubham.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -22,5 +22,13 @@ public class QuizController {
         return quizService.createQuiz(category,numQ,title);
     }
 
-    
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestion(id);
+    }
+
+    @PostMapping("/submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id,@RequestBody List<Response> responses){
+        return quizService.calculateResult(id,responses);
+    }
 }
